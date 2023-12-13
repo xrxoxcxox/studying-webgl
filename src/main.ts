b6b80c9de9f9ff70764f6a181cc6f81a33e9181a
Keisuke Watanuki <watanuki.keisuke@a-tm.co.jp>
2023-12-13 11:19:20 +0900

    Refactor WebGL shader program and buffer creation code

--- src/main.ts
+++ src/main.ts
@@ -1,123 +1,63 @@
+// canvas要素の取得とWebGL2コンテキストの取得
+const canvas = document.getElementById("webgl-canvas") as HTMLCanvasElement;
+const gl = canvas.getContext("webgl2") as WebGL2RenderingContext;
+
+// シェーダープログラムの作成
 import vertexShaderSource from "./shaders/plane.vert";
 import fragmentShaderSource from "./shaders/plane.frag";
 
-const canvas = document.getElementById("webgl-canvas");
-if (!(canvas instanceof HTMLCanvasElement)) {
-  throw new Error(
-    "The element with id 'webgl-canvas' is not an HTMLCanvasElement."
-  );
-}
+const vertexShader = gl.createShader(gl.VERTEX_SHADER) as WebGLShader;
+gl.shaderSource(vertexShader, vertexShaderSource);
+gl.compileShader(vertexShader);
 
-const gl = canvas.getContext("webgl2");
-if (!(gl instanceof WebGL2RenderingContext)) {
-  alert("Unable to initialize WebGL2.");
-  throw new Error("WebGL2 not supported");
-}
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER) as WebGLShader;
+gl.shaderSource(fragmentShader, fragmentShaderSource);
+gl.compileShader(fragmentShader);
 
-function setCanvasToDisplaySize(
-  canvas: HTMLCanvasElement,
-  gl: WebGL2RenderingContext
-) {
-  const displayWidth = canvas.clientWidth;
-  const displayHeight = canvas.clientHeight;
-  if (canvas.width !== displayWidth || canvas.height !== displayHeight) {
-    canvas.width = displayWidth;
-    canvas.height = displayHeight;
-    gl.viewport(0, 0, canvas.width, canvas.height);
-  }
-}
-
-window.addEventListener("resize", () => {
-  setCanvasToDisplaySize(canvas, gl);
-  drawScene(0, canvas, gl);
-});
-
-function setupShader(
-  source: string,
-  type: GLenum,
-  gl: WebGL2RenderingContext
-) {
-  const shader = gl.createShader(type);
-  if (!shader) {
-    console.error("Unable to create shader.");
-    return null;
-  }
-  gl.shaderSource(shader, source);
-  gl.compileShader(shader);
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    console.error(
-      "An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader)
-    );
-    gl.deleteShader(shader);
-    return null;
-  }
-  return shader;
-}
-
-const vertexShader = setupShader(vertexShaderSource, gl.VERTEX_SHADER, gl);
-const fragmentShader = setupShader(
-  fragmentShaderSource,
-  gl.FRAGMENT_SHADER,
-  gl
-);
-
-if (!vertexShader || !fragmentShader) {
-  console.error("Shaders were not created successfully");
-  throw new Error("Shaders were not created successfully");
-}
-
-const shaderProgram = gl.createProgram();
-if (!shaderProgram) {
-  console.error("Failed to create shader program");
-  throw new Error("Failed to create shader program");
-}
-
+const shaderProgram = gl.createProgram() as WebGLProgram;
 gl.attachShader(shaderProgram, vertexShader);
 gl.attachShader(shaderProgram, fragmentShader);
 gl.linkProgram(shaderProgram);
-
-if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-  console.error(
-    "Unable to initialize the shader program: " +
-      gl.getProgramInfoLog(shaderProgram)
-  );
-}
-
 gl.useProgram(shaderProgram);
 
-// prettier-ignore
-const vertices = new Float32Array([
-  // x, y
-  -1.0,  1.0,
+// バッファの作成と設定
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+// canvasの幅と高さの半分の大きさの矩形の頂点データ
+const positions = new Float32Array([
   -1.0, -1.0,
-   1.0,  1.0,
    1.0, -1.0,
+  -1.0,  1.0,
+   1.0,  1.0,
 ]);
+gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
 
-const vertexBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+const positionAttributeLocation = gl.getAttribLocation(
+  shaderProgram,
+  "aPosition"
+);
+gl.enableVertexAttribArray(positionAttributeLocation);
+gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
-const aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
-gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(aPosition);
-
-const uTime = gl.getUniformLocation(shaderProgram, "uTime");
-const uResolution = gl.getUniformLocation(shaderProgram, "uResolution");
-
-function drawScene(time: number, canvas: HTMLCanvasElement, gl: WebGL2RenderingContext) {
-  const currentTime = time * 0.001;
-  setCanvasToDisplaySize(canvas, gl);
-
-  gl.uniform1f(uTime, currentTime);
-  gl.uniform2f(uResolution, canvas.width, canvas.height);
-
+// 描画の実行
+function drawScene() {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
-
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-
-  requestAnimationFrame((time) => drawScene(time, canvas, gl));
 }
 
-drawScene(0, canvas, gl);
+// ウィンドウサイズに合わせてcanvasのサイズを変更する
+function resizeCanvasToDisplaySize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  drawScene();
+}
+
+// ウィンドウリサイズイベントに対するリスナーを設定
+window.addEventListener("resize", resizeCanvasToDisplaySize);
+
+// 初期表示時にもリサイズを実行
+document.addEventListener("DOMContentLoaded", resizeCanvasToDisplaySize);
+
--- src/shaders/plane.frag
+++ src/shaders/plane.frag
@@ -2,13 +2,11 @@
 
 precision mediump float;
 
-uniform float uTime;
-uniform vec2 uResolution;
-
+in vec2 vTextureCoord;
 out vec4 outColor;
 
 void main() {
-  float x = gl_FragCoord.x / uResolution.x;
-  float y = gl_FragCoord.y / uResolution.y;
-  outColor = vec4(x * abs(sin(uTime / 11.0f)), y * abs(sin(uTime / 13.0f)), abs(sin(uTime / 19.0f)), 1.0f);
+  float r = vTextureCoord.x;
+  float g = vTextureCoord.y;
+  outColor = vec4(r, g, 0.0, 1.0);
 }
