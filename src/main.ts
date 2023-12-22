a647144dc0f11d615ce6711b1106f7af3368b79e
Keisuke Watanuki <watanuki.keisuke@a-tm.co.jp>
2023-12-22 18:13:46 +0900

    Refactor scroll event in plane shader

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,6 @@
 const canvas = document.getElementById("webgl-canvas") as HTMLCanvasElement;
 const gl = canvas.getContext("webgl2") as WebGL2RenderingContext;
 
-let mousePosition = { x: 0, y: 0 };
-
-function getMousePosition(event: MouseEvent) {
-  mousePosition.x = event.clientX / canvas.width;
-  mousePosition.y = 1.0 - (event.clientY / canvas.height); // Y軸を反転
-}
-
-canvas.addEventListener('mousemove', getMousePosition);
-
 // シェーダープログラムの作成
 import vertexShaderSource from "./shaders/plane.vert";
 import fragmentShaderSource from "./shaders/plane.frag";
@@ -33,16 +24,14 @@ gl.useProgram(shaderProgram);
 const positionBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-// canvasの幅と高さの半分の大きさの矩形の頂点データ
+// canvasの幅と高さの大きさの矩形の頂点データ
 const positions = new Float32Array([
-  -1.0, -1.0,
-   1.0, -1.0,
-  -1.0,  1.0,
-   1.0,  1.0,
+  -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0,
 ]);
 gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
 
 // シェーダーに渡すデータの設定
+// 座標データ
 const positionAttributeLocation = gl.getAttribLocation(
   shaderProgram,
   "aPosition"
@@ -50,11 +39,91 @@ const positionAttributeLocation = gl.getAttribLocation(
 gl.enableVertexAttribArray(positionAttributeLocation);
 gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
-const resolutionUniformLocation = gl.getUniformLocation(shaderProgram, 'uResolution');
+// 解像度
+const resolutionUniformLocation = gl.getUniformLocation(
+  shaderProgram,
+  "uResolution"
+);
 
+// 時間
 const timeUniformLocation = gl.getUniformLocation(shaderProgram, "uTime");
 
-const mousePositionUniformLocation = gl.getUniformLocation(shaderProgram, "uMousePosition");
+// マウス位置
+const mousePositionUniformLocation = gl.getUniformLocation(
+  shaderProgram,
+  "uMousePosition"
+);
+
+let mousePosition = { x: 0, y: 0 };
+
+function getMousePosition(event: MouseEvent) {
+  mousePosition.x = event.clientX / canvas.width;
+  mousePosition.y = 1.0 - event.clientY / canvas.height; // Y軸を反転
+}
+
+window.addEventListener("mousemove", getMousePosition);
+
+// スクロール位置
+const scrollPositionUniformLocation = gl.getUniformLocation(
+  shaderProgram,
+  "uScrollPosition"
+);
+
+// sectionNumberの初期値設定
+let sectionNumber:number = 1;
+
+function updateSectionNumber(newSectionNumber: number) {
+  if (sectionNumber !== newSectionNumber) {
+    previousSectionNumber = sectionNumber;
+    sectionNumber = newSectionNumber;
+    interpolationFactor = 0.0; // セクションが変わったときに補完パラメータをリセット
+  }
+}
+
+// Intersection Observerのコールバック関数
+function intersectionObserverCallback(entries: IntersectionObserverEntry[]) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const sectionId = entry.target.getAttribute("id");
+      if (sectionId === "first-section") {
+        updateSectionNumber(1);
+      } else if (sectionId === "second-section") {
+        updateSectionNumber(2);
+      } else if (sectionId === "third-section") {
+        updateSectionNumber(3);
+      }
+    }
+  });
+}
+
+// Intersection Observerの設定
+const intersectionObserverOptions = {
+  root: null, // ビューポートをルートとする
+  rootMargin: "0px",
+  threshold: 0.1,
+};
+
+// Observerのインスタンス作成
+const observer = new IntersectionObserver(intersectionObserverCallback, intersectionObserverOptions);
+
+// 監視する要素を追加
+document.querySelectorAll(".section").forEach((section) => {
+  observer.observe(section);
+});
+
+// セクションのID
+const sectionNumberUniformLocation = gl.getUniformLocation(
+  shaderProgram,
+  "uSectionNumber"
+);
+
+let interpolationFactor = 0.0;
+let previousSectionNumber = 0;
+
+const interpolationFactorLocation = gl.getUniformLocation(
+  shaderProgram,
+  "uInterpolationFactor"
+);
 
 // 描画の実行
 function drawScene(width: number, height: number, time: number) {
@@ -62,6 +131,12 @@ function drawScene(width: number, height: number, time: number) {
   gl.uniform1f(timeUniformLocation, currentTime);
   gl.uniform2f(resolutionUniformLocation, width, height);
   gl.uniform2f(mousePositionUniformLocation, mousePosition.x, mousePosition.y);
+  gl.uniform1f(scrollPositionUniformLocation, window.scrollY);
+  gl.uniform1i(sectionNumberUniformLocation, sectionNumber);
+  if (sectionNumber !== previousSectionNumber && interpolationFactor < 1.0) {
+    interpolationFactor += 0.01; // 補完速度調整
+    gl.uniform1f(interpolationFactorLocation, interpolationFactor);
+  }
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
--- src/shaders/plane.frag
+++ src/shaders/plane.frag
@@ -2,69 +2,38 @@
 
 precision mediump float;
 
-uniform float uTime;
 uniform vec2 uResolution;
+uniform float uTime;
 uniform vec2 uMousePosition;
+uniform float uScrollPosition;
+uniform int uSectionNumber;
+uniform float uInterpolationFactor;
 
 out vec4 outColor;
-
-float pi = acos(-1.0);
-
-vec3 permute(vec3 x) {
-  return mod(((x * 34.0) + 1.0) * x, 289.0);
-}
-
-float sNoise(vec2 v) {
-  const vec4 C = vec4(0.211324865405187, 0.366025403784439, -0.577350269189626, 0.024390243902439);
-  vec2 i = floor(v + dot(v, C.yy));
-  vec2 x0 = v - i + dot(i, C.xx);
-  vec2 i1;
-  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
-  vec4 x12 = x0.xyxy + C.xxzz;
-  x12.xy -= i1;
-  i = mod(i, 289.0);
-  vec3 p = permute(permute(i.y + vec3(0.0, i1.y, 1.0)) + i.x + vec3(0.0, i1.x, 1.0));
-  vec3 m = max(0.5 - vec3(dot(x0, x0), dot(x12.xy, x12.xy), dot(x12.zw, x12.zw)), 0.0);
-  m = m * m;
-  m = m * m;
-  vec3 x = 2.0 * fract(p * C.www) - 1.0;
-  vec3 h = abs(x) - 0.5;
-  vec3 ox = floor(x + 0.5);
-  vec3 a0 = x - ox;
-  m *= 1.79284291400159 - 0.85373472095314 * (a0 * a0 + h * h);
-  vec3 g;
-  g.x = a0.x * x0.x + h.x * x0.y;
-  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
-  return 130.0 * dot(m, g);
-}
 
 void main() {
   vec2 uv = gl_FragCoord.xy / uResolution;
 
-  // アスペクト比を考慮したUV座標の計算
-  vec2 aspectCorrectedUV = uv;
-  aspectCorrectedUV.x *= uResolution.x / uResolution.y;
+  // outColor = vec4(uv.x, sin(uScrollPosition * 0.001), uMousePosition.y, 1.0);
 
-  // マウス位置のアスペクト比を補正
-  vec2 aspectCorrectedMouse = uMousePosition;
-  aspectCorrectedMouse.x *= uResolution.x / uResolution.y;
+  vec4 colorA;
+  vec4 colorB;
 
-  // マウス位置との距離を計算
-  float mouseDistnace = distance(aspectCorrectedMouse, aspectCorrectedUV);
-  float mouseColorIntensity = 1.0 - smoothstep(0.0, 500.0 / uResolution.x, mouseDistnace);
-  float mouseR = mouseColorIntensity * 0.5;
-  float mouseG = mouseColorIntensity;
-  float mouseB = mouseColorIntensity * 0.8;
-  float mouseA = mouseColorIntensity * 0.2;
-  vec4 mouseColor = vec4(mouseR, mouseG, mouseB, mouseA);
+  vec4 firstColor = vec4(abs(sin(uv.x + uTime)), 0.0, 0.0, 1.0);
+  vec4 secondColor = vec4(0.0, abs(sin(uv.y + uTime)), 0.0, 1.0);
+  vec4 thirdColor = vec4(0.0, 0.0, abs(sin(uv.x + uv.y + uTime)), 1.0);
 
-  float offset = sNoise(vec2(cos(uv.x * pi) + uTime * 0.1, uv.y * sin(uTime) * 0.2) - uTime * 0.1);
-  float colorValue = mix(1.0, offset, 0.25);
-  float r = clamp(colorValue, 0.2, 0.75);
-  float g = clamp(colorValue + 0.1, 0.2, 0.85);
-  float b = clamp(colorValue * 1.2, 0.2, 0.9);
+  // セクションに基づいた色の設定
+  if (uSectionNumber == 1) {
+    colorA = firstColor;
+    colorB = firstColor;
+  } else if (uSectionNumber == 2) {
+    colorA = firstColor;
+    colorB = secondColor;
+  } else if (uSectionNumber == 3) {
+    colorA = secondColor;
+    colorB = thirdColor;
+  }
 
-  vec4 gradient = vec4(r, g, b, 1.0);
-
-  outColor = mix(gradient, mouseColor, mouseColor.a);
+  outColor = mix(colorA, colorB, uInterpolationFactor);
 }
